dc307228bd198be1d0e1a15323bafa76e34546d2
akylem100 <akylem@byui.edu>
2024-05-18 17:31:44 -0600

    fixed cool pics part 1

--- coolpics/scripts/cool-pics.js
+++ coolpics/scripts/cool-pics.js
@@ -1,1 +1,15 @@
 const year = document.querySelector('#year').innerHTML = new Date().getFullYear();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menubutton = document.getElementById('menubutton');
+    var menu = document.querySelector('.navbarelements');
+
+    menubutton.addEventListener('click', function() {
+        if (menu.style.display === 'block') {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'block';
+        }
+    });
+}); 
