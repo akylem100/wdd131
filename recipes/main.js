cc0acaeda75292e8a2d1afae2f3420119e61ec21
akylem100 <akylem@byui.edu>
2024-07-20 23:11:24 -0600

    last recipe fix before turn in

--- recipes/main.js
+++ recipes/main.js
@@ -37,28 +37,28 @@ function ratingTemplate(rating) {
     return stars;
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const recipesContainer = document.querySelector('.recipescontainer');
-//     console.log('recipesContainer:', recipesContainer);
+document.addEventListener('DOMContentLoaded', () => {
+    const recipesContainer = document.querySelector('.recipescontainer');
+    console.log('recipesContainer:', recipesContainer);
 
-//     const createRecipeElement = (recipe) => {
-//         const recipeElement = document.createElement('div');
-//         recipeElement.classList.add('recipe');
+    const createRecipeElement = (recipe) => {
+        const recipeElement = document.createElement('div');
+        recipeElement.classList.add('recipe');
         
-//         recipeElement.innerHTML = `
-//             <section class="recipe-box">
-//             <img src="${recipe.image}" width="300px" alt="${recipe.name}">
-//             <h3>${recipe.tags}</h3>
-//             <h2>${recipe.name}</h2><br>
-//             <p>${recipe.description}</p>
-//         `;
+        recipeElement.innerHTML = `
+            <section class="recipe-box">
+            <img src="${recipe.image}" width="300px" alt="${recipe.name}">
+            <h3>${recipe.tags}</h3>
+            <h2>${recipe.name}</h2><br>
+            <p>${recipe.description}</p>
+        `;
         
-//         return recipeElement;
-//     }
+        return recipeElement;
+    }
 
-//     recipes.forEach(recipe => {
-//         console.log('Creating recipe element for:', recipe.name);
-//         const recipeElement = createRecipeElement(recipe);
-//         recipesContainer.appendChild(recipeElement);
-//     });
-// });
+    recipes.forEach(recipe => {
+        console.log('Creating recipe element for:', recipe.name);
+        const recipeElement = createRecipeElement(recipe);
+        recipesContainer.appendChild(recipeElement);
+    });
+});
